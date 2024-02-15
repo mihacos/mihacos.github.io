2bab6b5eaa93bbdac8fbb80f92d69073796733e2
Mhaela <mihaela.anamaria.costea@gmail.com>
2024-02-15 18:34:56 +0200

    show all pages

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
+function showHome() {
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+
+  var page = document.getElementById("home");
+  page.style.display = "block";
+}
+
+function showSkills() {
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("home").style.display = "none";
+
+  var page = document.getElementById("skills");
+  page.style.display = "block";
+}
+
 function showLanguages() {
-  var oldPage = document.getElementById("projects");
-  oldPage.style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
 
   var page = document.getElementById("languages");
+  page.style.display = "block";
+}
+
+function showProjects() {
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+
+  var page = document.getElementById("projects");
   page.style.display = "block";
 }
